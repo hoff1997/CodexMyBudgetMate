61b3ebcfdcb7cd0f77d0ef3cd9ba0b5f14af6a59
hoff1997 <hoff1997@gmail.com>
2025-10-28 15:06:02 +1300

    Restore main layout for envelope planning

--- app/envelope-planning/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import Sidebar from "@/components/layout/sidebar";
-
-export default function EnvelopePlanningLayout({ children }: { children: ReactNode }) {
-  return <Sidebar>{children}</Sidebar>;
-}
